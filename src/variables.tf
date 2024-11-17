d256a5e64de31d1f713a3acf81906e8ad021c6dc
AmitAnveri <anveri.a@northeastern.edu>
2024-11-17 10:41:08 -0500

    formatting

--- src/variables.tf
+++ src/variables.tf
@@ -18,17 +18,17 @@ variable "vpc_cidr" {
 
 variable "public_subnet_cidrs" {
   description = "CIDR blocks for public subnets"
-  default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
 
 variable "private_subnet_cidrs" {
   description = "CIDR blocks for private subnets"
-  default = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  default     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 }
 
 variable "availability_zones" {
   description = "Availability zones for the subnets"
-  default = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
 
 variable "internet_gateway_cidr" {
